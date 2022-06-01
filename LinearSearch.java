b78d736e42f0d899a0f7fdd13dc00fbdf1540561
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-06-01 15:51:27 -0400

    Update and rename Searching.java to LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,6 @@
 
 
-public class Searching {
+public class LinearSearch {
     public static boolean linearSearch(int[] arr, int key) {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == key) {
