62f5d89bbec83e2372f7030560aa73c6bac2ce36
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-06-07 15:16:13 -0400

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -4,86 +4,116 @@ package com.mycompany.testlinkedlist;
 
 public class SinglyLinkedList<E> {
     
-        public static class Node<E> {
-            private E element;
-            private Node<E> next;
-             
-             
-            public Node(E element, Node<E> next) {
-                this.element = element;
-                this.next = next;
-            }
-             
-            public E getElement() {
-                return element;
-                 
-            }
-             
-            public Node<E> getNext() {
-                return next;
-             }
-             
-            public void setNextNode(Node<E> next) {
-                this.next = next;
-            }     
+        public class SinglyLinkedList<E> {
+
+    private static class Node<E> {
+
+        private E element;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
         }
-        
-        private Node<E> head = null;
-        private Node<E> tail = null;
-        int size = 0;
-        
-        public SinglyLinkedList() {
-            
+
+        public E getElement() {
+            return element;
+
         }
-        
-        public int size() {
-            return size;
+
+        public Node<E> getNext() {
+            return next;
         }
-        
-        public boolean isEmpty() {
-            return size == 0;
+
+        public void setNextNode(Node<E> next) {
+            this.next = next;
         }
-        
-        public E first() {
-            return head.getElement();
+    }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    int size = 0;
+
+    public SinglyLinkedList() {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        return head.getElement();
+    }
+
+    public E last() {
+        return tail.getElement();
+    }
+
+    public void addFirst(E e) {
+        head = new Node<>(e, head);
+        if (isEmpty()) {
+            tail = head;
         }
-        
-        public E last() {
-            return tail.getElement();
+        size++;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node<>(e, null);
+        if (isEmpty()) {
+            head = newest;
+        } else {
+            tail.setNextNode(newest);
         }
-        
-        public void addFirst(E e) {
-            head = new Node<>(e,head);
-            if(isEmpty()) {
-                tail = head;
-            }
-            size++;
+        tail = newest;
+        size++;
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
         }
-        
-        public void addLast(E e) {
-            Node<E> newest = new Node<>(e,null);
-            if(isEmpty()) {
-                head = newest;
-            }
-            else {
-                tail.setNextNode(newest);
-            }
-            tail = newest;
-            size++;
+        E answer = head.getElement();
+        head = head.getNext();
+        size--;
+        if (isEmpty()) {
+            tail = null;
         }
-        
-        public E removeFirst() {
-            if(isEmpty()) {
-                return null;
-            }
-            E answer = head.getElement();
-            head = head.getNext();
-            size--;
-            if(isEmpty()) {
-                tail = null;
-            }
-            return answer;
+        return answer;
+    }
+
+    public E removeLast() {
+        if (isEmpty()) {
+            return null;
         }
+        if (size == 1) {
+            return removeFirst();
+        }
+
+        Node<E> temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        E answer = temp.getElement();
+        temp.setNextNode(null);
+        tail = temp;
+
+        return answer;
+    }
+    
+    public void traverse() {
+        Node<E> temp = head;
+        while(temp != null) {
+            System.out.print(temp.getElement() + " ");
+            temp = temp.next;
+        }
+    }
+}
        
+     
    
 }
